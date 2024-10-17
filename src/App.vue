462cfd709832b83ed164c5b28c5b6f2fc6797027
Barsukov Dmitrii <d.barsukov@matras.ru>
2024-10-17 21:58:04 +0400

    Refactor Vuetify configuration and dependencies
    - Refactor Vuetify configuration in 'src/plugins/vuetify.js'
    - Update transpile dependencies in 'vue.config.js'
    - Import Vuetify in 'src/main.js'
    - Add Vuetify logo in 'src/assets/logo.svg'
    - Update dependencies in 'package.json'
    - Add Google Fonts and Material Design Icons in 'public/index.html'
    - Update Vuetify component in 'src/App.vue'
    - Update HelloWorld component in 'src/components/HelloWorld.vue'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<v-app>
+		<v-app-bar app color="primary" dark>
+			<div class="d-flex align-center">
+				<v-img
+					alt="Vuetify Logo"
+					class="shrink mr-2"
+					contain
+					src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+					transition="scale-transition"
+					width="40"
+				/>
+
+				<v-img
+					alt="Vuetify Name"
+					class="shrink mt-1 hidden-sm-and-down"
+					contain
+					min-width="100"
+					src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+					width="100"
+				/>
+			</div>
+
+			<v-spacer></v-spacer>
+
+			<v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
+				<span class="mr-2">Latest Release</span>
+				<v-icon>mdi-open-in-new</v-icon>
+			</v-btn>
+		</v-app-bar>
+
+		<v-main>
+			<v-container class="d-flex justify-center align-center">
+				<div>
+					<h3 class="text-center my-4">Calendar Test</h3>
+					<v-date-picker
+						v-model="date"
+						@contextmenu:year="contextMenu"
+						@dblclick:date="dblClick"
+						@mouseenter:month="mouseEnter"
+						@mouseleave:month="mouseLeave"
+					></v-date-picker>
+					<!-- <HelloWorld/> -->
+				</div>
+			</v-container>
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+
+	components: {
+		// HelloWorld,
+	},
+
+	data: () => ({
+		date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+			.toISOString()
+			.substr(0, 10),
+		done: [false, false, false],
+		mouseMonth: null,
+	}),
+
+	methods: {
+		contextMenu(year, event) {
+			this.$set(this.done, 2, true)
+
+			event.preventDefault()
+
+			alert(`You have activated context menu for year ${year}`)
+		},
+		dblClick(date) {
+			this.$set(this.done, 0, true)
+
+			alert(`You have just double clicked the following date: ${date}`)
+		},
+		mouseEnter(month) {
+			this.$set(this.done, 1, true)
+			this.mouseMonth = month
+		},
+		mouseLeave() {
+			this.mouseMonth = null
+		},
+	},
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
